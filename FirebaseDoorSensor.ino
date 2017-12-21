5299ed786f1e317d91413377f2325b5d60ef1081
James Finefrock <finefrockjames@gmail.com>
2017-12-21 15:42:23 -0500

    Implemented basic FCM

--- FirebaseDoorSensor.ino
+++ FirebaseDoorSensor.ino
@@ -1,8 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
 
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
+
+#define FCM_SERVER_KEY ""
+#define FCM_TOPIC ""
 
 #define FIREBASE_HOST ""
 #define FIREBASE_AUTH ""
@@ -24,10 +28,10 @@ void setup() {
   connectWifi();
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
   initializeDB();
+  sendFCM();
 
-  pinMode(switchPin, INPUT);
+  //pinMode(switchPin, INPUT);
 }
 
 void loop() {
@@ -59,5 +63,40 @@ void initializeDB() {
   Firebase.setString(FB_PATH DOOR_ID NAME_PATH, DOOR_NAME);
   Firebase.setBool(FB_PATH DOOR_ID OPEN_PATH, false);
   Firebase.setString(FB_PATH DOOR_ID LASTOPENED_PATH, "");
+}
+
+void sendFCM() {
+  WiFiClientSecure client;
+  
+  String data = "{";
+  data = data + "\"to\": \"/topics/security\",";
+  data = data + "\"data\": {";
+  data = data + "\"Nick\": \"asdsadsa\",";
+  data = data + "\"Body\" : \"Some Test\" ";
+  data = data + "} }";
+  
+  Serial.println("Send data...");
+  
+  if (client.connect("fcm.googleapis.com", 443)) {
+    Serial.println("Connected to the server..");
+    client.println("POST /fcm/send HTTP/1.1");
+    client.println("Host: fcm.googleapis.com");
+    client.println("Authorization: key=" FCM_SERVER_KEY);
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.println(data);
+  }
+  
+  Serial.println("Data sent...Reading response..");
+  
+  while (client.available()) {
+   char c = client.read();
+   Serial.print(c);
+  }
+  Serial.println("Finished!");
+  client.flush();
+  client.stop();
 }
 
