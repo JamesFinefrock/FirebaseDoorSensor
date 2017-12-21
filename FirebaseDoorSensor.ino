c39b37d30d2c7d439e17d4e2723d1b95431ff2e2
James Finefrock <finefrockjames@gmail.com>
2017-12-21 13:49:45 -0500

    More constants for FB configuration

--- FirebaseDoorSensor.ino
+++ FirebaseDoorSensor.ino
@@ -3,9 +3,18 @@
 
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
+
 #define FIREBASE_HOST ""
 #define FIREBASE_AUTH ""
 #define FB_PATH ""
+#define ID_PATH ""
+#define NAME_PATH ""
+#define OPEN_PATH ""
+#define LASTOPENED_PATH ""
+
+#define DOOR_ID ""
+#define DOOR_NAME ""
+
 
 const int switchPin = 1;
 int lastState = LOW;
@@ -13,7 +22,26 @@ int lastState = LOW;
 void setup() {
   Serial.begin(115200);
 
-  // connect to wifi.
+  connectWifi();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  int curState = digitalRead(switchPin);
+  
+  if(curState != lastState) {
+    boolean isOpen = curState == HIGH ? true : false;
+    Firebase.setBool(FB_PATH DOOR_ID OPEN_PATH, isOpen);
+    lastState = curState;
+  }
+  
+  delay(500);
+}
+
+void connectWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -23,19 +51,5 @@ void setup() {
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  pinMode(switchPin, INPUT);
 }
 
-void loop() {
-  int curState = digitalRead(switchPin);
-  if(curState != lastState) {
-    boolean isOpen = curState == HIGH ? true : false;
-    Firebase.setBool(FB_PATH, isOpen);
-    lastState = curState;
-  }
-  
-  delay(500);
-}
